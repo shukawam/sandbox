1f3dd25fae873e5fd253424b962e9ae22b3c05ca
s001-kawamura <s001.kawamura@gmail.com>
2020-12-01 10:39:52 +0000

    Update

--- http-basic-authn/src/main/java/com/example/fn/HelloFunction.java
+++ http-basic-authn/src/main/java/com/example/fn/HelloFunction.java
@@ -6,44 +6,46 @@ import java.util.Map;
 
 public class HelloFunction {
 
-    private static final String OK_USER = "test001@example.com:password123";
+    private static final String SAMPLE_USER = "test001@example.com:password123";
 
     private static final String TYPE = "TOKEN";
 
     public Result handleRequest(Input input) {
-        final var invalidResult = invalidatedResult();
-        if ("".equals(input.getType()) || !TYPE.equals(input.getType())) {
+        final var invalidResult = invalidResult();
+        if (!checkInput(input)) {
             return invalidResult;
         }
-        if (input.getToken() == null || "".equals(input.getToken())) {
-            return invalidResult;
-        }
-        var expectedToken = Base64.getEncoder().encodeToString(OK_USER.getBytes(StandardCharsets.UTF_8));
+        final var expectedToken = Base64.getEncoder().encodeToString(SAMPLE_USER.getBytes(StandardCharsets.UTF_8));
         if (!expectedToken.equals(input.getToken().replaceAll("Basic\\s", ""))) {
             System.out.println("Basic Authentication is failed!");
             return invalidResult;
         }
-        return validatedResult();
+        System.out.println("Basic Authentication is success!");
+        return validResult();
     }
 
-    private static Result validatedResult() {
-        var validatedResult = new Result();
-        validatedResult.setActive(true);
-        validatedResult.setExpiresAt("2019-05-30T10:15:30+01:00");
-        validatedResult.setPrincipal("https://example.com/users/jdoe");
-        validatedResult.setScope(new String[]{"list:hello", "read:hello", "create:hello", "update:hello", "delete:hello", "someScope"});
-        validatedResult.setClientId("host123");
-        validatedResult.setContext(Map.of("email", "john.doe@example.com"));
-        return validatedResult;
+    private static boolean checkInput(Input input) {
+        return  input == null || "".equals(input.getType()) || !TYPE.equals(input.getType()) || input.getToken() == null || "".equals(input.getToken()) ? false : true;
     }
 
-    private static Result invalidatedResult() {
-        var invalidatedResult = new Result();
-        invalidatedResult.setActive(false);
-        invalidatedResult.setExpiresAt("2019-05-30T10:15:30+01:00");
-//        invalidatedResult.setContext(Map.of("email", "john.doe@example.com"));
-        invalidatedResult.setWwwAuthenticate("Basic realm=\"example.com\"");
-        return invalidatedResult;
+    private static Result validResult() {
+        var validResult = new Result();
+        validResult.setActive(true);
+        validResult.setExpiresAt("2019-05-30T10:15:30+01:00");
+        validResult.setPrincipal("https://example.com/users/jdoe");
+        validResult.setScope(new String[]{"list:hello", "read:hello", "create:hello", "update:hello", "delete:hello", "someScope"});
+        validResult.setClientId("host123");
+        validResult.setContext(Map.of("email", "john.doe@example.com"));
+        return validResult;
+    }
+
+    private static Result invalidResult() {
+        var invalidResult = new Result();
+        invalidResult.setActive(false);
+        invalidResult.setExpiresAt("2019-05-30T10:15:30+01:00");
+//        invalidResult.setContext(Map.of("email", "john.doe@example.com"));
+        invalidResult.setWwwAuthenticate("Basic realm=\"example.com\"");
+        return invalidResult;
     }
 
 }
