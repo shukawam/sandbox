995676231a506d23349286fc5c84fd569dc8ce8a
s001-kawamura <s001.kawamura@gmail.com>
2020-12-04 14:24:41 +0000

    Delete sign-in component

--- idcs-auth-sample-client/src/app/app-routing.module.ts
+++ idcs-auth-sample-client/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ItemCreateComponent } from './items/item-create/item-create.component';
 import { ItemSearchComponent } from './items/item-search/item-search.component';
-import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '/sign-in', component: SignInComponent },
-  { path: '/item-search', component: ItemSearchComponent },
-  { path: '/item-create', component: ItemCreateComponent }
+  { path: 'item-search', component: ItemSearchComponent },
+  { path: 'item-create', component: ItemCreateComponent }
 ];
 
 @NgModule({
