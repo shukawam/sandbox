3dc1f287200cc86108077d84c072770afd7aff4e
s001-kawamura <s001.kawamura@gmail.com>
2020-12-04 11:28:29 +0900

    Update

--- idcs-authn/src/main/java/com/example/fn/HelloFunction.java
+++ idcs-authn/src/main/java/com/example/fn/HelloFunction.java
@@ -1,12 +1,42 @@
 package com.example.fn;
 
+import java.util.Map;
+
 public class HelloFunction {
 
-    public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
+    private static final String TYPE = "TOKEN";
 
-        System.out.println("Inside Java Hello World function"); 
-        return "Hello, " + name + "!";
+    public Result handleRequest(Input input) {
+        var invalidResult = invalidResult();
+        if (!checkInput(input)) {
+            return invalidResult;
+        }
+        System.out.println("*** Token ***");
+        System.out.println(input.getToken());
+        return invalidResult;
     }
 
+    private static boolean checkInput(Input input) {
+        return input == null || "".equals(input.getType()) || !TYPE.equals(input.getType()) || input.getToken() == null || "".equals(input.getToken()) ? false : true;
+    }
+
+    private static Result validResult() {
+        var validResult = new Result();
+        validResult.setActive(true);
+        validResult.setExpiresAt("2019-05-30T10:15:30+01:00");
+        validResult.setPrincipal("https://example.com/users/jdoe");
+        validResult.setScope(new String[]{"list:hello", "read:hello"});
+        validResult.setClientId("host123");
+        validResult.setContext(Map.of("email", "john.doe@example.com"));
+        return validResult;
+    }
+
+    private static Result invalidResult() {
+        var invalidResult = new Result();
+        invalidResult.setActive(false);
+        invalidResult.setExpiresAt("2019-05-30T10:15:30+01:00");
+//        invalidResult.setContext(Map.of("email", "john.doe@example.com"));
+        invalidResult.setWwwAuthenticate("Basic realm=\"example.com\"");
+        return invalidResult;
+    }
 }
