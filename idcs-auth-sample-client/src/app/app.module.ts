5c8e874d803d1775bce12cc12a251a6aa1176238
s001-kawamura <s001.kawamura@gmail.com>
2020-12-04 14:25:27 +0000

    Add http client module

--- idcs-auth-sample-client/src/app/app.module.ts
+++ idcs-auth-sample-client/src/app/app.module.ts
@@ -5,22 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
-import { SignInComponent } from './sign-in/sign-in.component';
 import { ItemSearchComponent } from './items/item-search/item-search.component';
 import { ItemCreateComponent } from './items/item-create/item-create.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignInComponent,
     ItemSearchComponent,
     ItemCreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    // import HttpClientModule after BrowserModule.
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
