876f556b6e6d22ae82a6d5c5896ac9178bce72ed
s001-kawamura <s001.kawamura@gmail.com>
2020-12-15 15:44:34 +0000

    Delete apigw-functions-demo-app-front

--- apigw-functions-demo-app-front/src/app/interface/get-access-token-response-body.ts
+++
@@ -1,6 +0,0 @@
-export interface GetAccessTokenResponseBody {
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-    id_token: string;
-}
--- apigw-functions-demo-app-front/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  idcs_base_url: 'https://idcs-f0017ddeef0241cfbd70e057c032ff44.identity.oraclecloud.com',
-  client_id: 'ae777aecc5bb4d83a511990c465238b3',
-  client_secret: '5f77ab91-24e9-4a55-93e2-8a2e498f90c1',
-  base64url_encoded_credential: 'YWU3NzdhZWNjNWJiNGQ4M2E1MTE5OTBjNDY1MjM4YjM6NWY3N2FiOTEtMjRlOS00YTU1LTkzZTItOGEyZTQ5OGY5MGMx',
-  item_base: 'https://fgw5zmlsdxpqyppd4mwure4zdy.apigateway.ap-tokyo-1.oci.customer-oci.com/api/v1/items'
-};
