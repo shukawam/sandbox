e98a4b4e767717dbc3aea2c03c9bc6d1755ed89a
s001-kawamura <s001.kawamura@gmail.com>
2020-12-15 12:48:49 +0000

    Update

--- idcs-auth-sample-client/src/app/items/item-search/item-search.component.ts
+++ idcs-auth-sample-client/src/app/items/item-search/item-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Item } from 'src/app/interface/item';
 import { ItemSearchService } from 'src/app/service/items/item-search.service';
 
 @Component({
@@ -14,6 +15,8 @@ export class ItemSearchComponent implements OnInit {
   private code: string = '';
 
   private accessToken: string = '';
+
+  items: Item[] = null;
 
   constructor(private readonly router: Router, private readonly itemSearchService: ItemSearchService) { }
 
@@ -30,6 +33,16 @@ export class ItemSearchComponent implements OnInit {
       console.log(this.accessToken);
     }).catch(error => {
       throw new Error(error)
+    });
+  }
+
+  async itemSearch(): Promise<void> {
+    await this.itemSearchService.getAllItems().then(data => {
+      console.log('Show Response Body.');
+      console.log(data);
+      this.items = data;
+    }).catch(error => {
+      throw new Error(error);
     });
   }
 
