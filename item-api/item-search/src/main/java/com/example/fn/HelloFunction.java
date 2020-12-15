dfffaed5b04ad1f7df88381bd0f39daf14c20809
s001-kawamura <s001.kawamura@gmail.com>
2020-12-15 12:49:44 +0900

    Update

--- item-api/item-search/src/main/java/com/example/fn/HelloFunction.java
+++ item-api/item-search/src/main/java/com/example/fn/HelloFunction.java
@@ -1,9 +1,83 @@
 package com.example.fn;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
 public class HelloFunction {
 
-    public Item[] handleRequest() {
-        return null;
+    private static final String ORDS_BASE_URL = "ords_base_url";
+
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public CompletableFuture<Map<String, String>> handleRequest() {
+        final var ordsBaseUrl = System.getenv(ORDS_BASE_URL);
+        var httpRequest = HttpRequest.newBuilder(URI.create(ordsBaseUrl))
+                .header("Authorization", getAuthToken())
+                .GET()
+                .build();
+        UncheckedObjectMapper objectMapper = new UncheckedObjectMapper();
+        return httpClient
+                .sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(objectMapper::readValue);
+
     }
 
+    private String getAuthToken() {
+        String authToken = "";
+        try {
+            var ordsBaseUrl = System.getenv("ords_base_url");
+            var clientId = System.getenv("client_id");
+            var clientSecret = System.getenv("client_secret");
+            System.out.println("ordsBaseUrl:" + ordsBaseUrl);
+            System.out.println("clientId:" + clientId);
+            System.out.println("clientSecret:" + clientSecret);
+
+            var authString = clientId + ":" + clientSecret;
+            var authEncoded = "Basic " + Base64.getEncoder().encodeToString(authString.getBytes());
+            System.out.println("oauth url:" + ordsBaseUrl + "/oauth/token");
+            var request = HttpRequest.newBuilder(URI.create(ordsBaseUrl + "/oauth/token"))
+                    .header("Authorization", authEncoded)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
+                    .build();
+            var response = this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("response:" + response);
+            String responseBody = response.body();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<HashMap<String, String>> typeRef = new TypeReference<HashMap<String, String>>() {
+            };
+            HashMap<String, String> result = mapper.readValue(responseBody, typeRef);
+            authToken = result.get("access_token");
+            System.out.println("authToken:" + authToken);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return authToken;
+    }
+
+}
+
+class UncheckedObjectMapper extends com.fasterxml.jackson.databind.ObjectMapper {
+    /**
+     * Parses the given JSON string into a Map.
+     */
+    Map<String, String> readValue(String content) {
+        try {
+            return this.readValue(content, new TypeReference<>() {
+            });
+        } catch (IOException ioe) {
+            throw new CompletionException(ioe);
+        }
+    }
 }
