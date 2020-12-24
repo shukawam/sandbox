0d3c1810976804735e202cbf9508e8473ab9704d
s001-kawamura <s001.kawamura@gmail.com>
2020-12-24 02:04:51 +0000

    Implemented simple web server

--- api-gateway-verification/request-response-transforming/simple-api-express/server.js
+++ api-gateway-verification/request-response-transforming/simple-api-express/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(30000);
 
 app.get('/hello', (req, res) => {
-    console.log('Incoming request headers.');
+    console.log(`Incoming request headers. ${Date.now()}`);
     console.log(req.headers);
-    res.send('Hello World!');
+    res.send(`Hello ${req.query.name}`);
 });
