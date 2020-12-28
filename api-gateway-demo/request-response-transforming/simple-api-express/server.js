a49ab5a4984ffe1bd4a73306cfc33085a5cf5f0d
s001-kawamura <s001.kawamura@gmail.com>
2020-12-28 15:59:06 +0000

    Update express server

--- api-gateway-demo/request-response-transforming/simple-api-express/server.js
+++ api-gateway-demo/request-response-transforming/simple-api-express/server.js
@@ -6,8 +6,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(30000);
 
-app.get('/hello', (req, res) => {
-    console.log(`Incoming request headers. ${Date.now()}`);
+// Simply shows HTTP Request Headers and Query Parameters.
+app.get('/header-query-logging', (req, res) => {
+    const now = Date.now();
+    console.log(`${now}: Request Headers`);
     console.log(req.headers);
+    if ('required-header' in req.headers) {
+        console.log('ok');
+    }
+    console.log(`${now}: Request Query`);
+    console.log(req.query);
     res.send(`Hello ${req.query.name}`);
+});
+
+// Requires specific HTTP Request Header(required-header).
+app.get('/require-header', (req, res) => {
+    console.log('required-header' in req.headers);
+    if ('required-header' in req.headers === false) {
+        console.log('Missing required-header in HTTP Request Headers.');
+        res.send('Missing required-header in HTTP Request Headers.');
+    } else {
+        res.send(`Include specific HTTP Request Header!`);
+    }
+});
+
+// Delete "Server" Header.
+app.get('/delete-server-header', (req, res) => {
+    res.send('Hello');
 });
