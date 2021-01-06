245843d37a167e840124637c7ab7296079a5b79c
s001-kawamura <s001.kawamura@gmail.com>
2021-01-06 22:00:04 +0900

    Update HTTP Basic Authn

--- http-basic-authn/src/main/java/com/example/fn/HelloFunction.java
+++ http-basic-authn/src/main/java/com/example/fn/HelloFunction.java
@@ -1,51 +1,56 @@
 package com.example.fn;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Map;
 
 public class HelloFunction {
 
-    private static final String SAMPLE_USER = "test001@example.com:password123";
+    private static final String TOKEN_PREFIX = "Basic ";
 
-    private static final String TYPE = "TOKEN";
+    private static final String USER = "sample";
+
+    private static final String PASSWORD = "password";
+
+    public static class Input {
+        public String type;
+        public String token;
+    }
+
+    public static class Result {
+        // required
+        public boolean active = false;
+        public String principal;
+        public String[] scope;
+        public String expiresAt;
+        // optional
+        public String wwwAuthenticate;
+        // optional
+        public String clientId;
+        // optional context
+        public Map<String, Object> context;
+    }
 
     public Result handleRequest(Input input) {
-        final var invalidResult = invalidResult();
-        if (!checkInput(input)) {
-            return invalidResult;
+        var result = new Result();
+        if (input.token == null || !input.token.startsWith(TOKEN_PREFIX)) {
+            result.active = false;
+            result.expiresAt = "2021-01-10T10:15:30+01:00";
+            result.wwwAuthenticate = "Basic error=\"missing_token\"";
+            return result;
         }
-        final var expectedToken = Base64.getEncoder().encodeToString(SAMPLE_USER.getBytes(StandardCharsets.UTF_8));
-        if (!expectedToken.equals(input.getToken().replaceAll("Basic\\s", ""))) {
-            System.out.println("Basic Authentication is failed!");
-            return invalidResult;
+        var correctToken = Base64.getEncoder().encode(String.format("%s:%s", USER, PASSWORD).getBytes());
+        if (!correctToken.equals(input.token.substring(TOKEN_PREFIX.length()))) {
+            result.active = false;
+            result.expiresAt = "2021-01-10T10:15:30+01:00";
+            result.wwwAuthenticate = "Basic error=\"invalid_token\"";
+            return result;
         }
-        System.out.println("Basic Authentication is success!");
-        return validResult();
+        result.active = true;
+        result.expiresAt = "2021-01-10T10:15:30+01:00\"";
+        result.principal = USER;
+        result.scope = new String[]{"list:hello", "create:hello"};
+        return result;
     }
 
-    private static boolean checkInput(Input input) {
-        return  input == null || "".equals(input.getType()) || !TYPE.equals(input.getType()) || input.getToken() == null || "".equals(input.getToken()) ? false : true;
-    }
-
-    private static Result validResult() {
-        var validResult = new Result();
-        validResult.setActive(true);
-        validResult.setExpiresAt("2019-05-30T10:15:30+01:00");
-        validResult.setPrincipal("https://example.com/users/jdoe");
-        validResult.setScope(new String[]{"list:hello", "read:hello"});
-        validResult.setClientId("host123");
-        validResult.setContext(Map.of("email", "john.doe@example.com"));
-        return validResult;
-    }
-
-    private static Result invalidResult() {
-        var invalidResult = new Result();
-        invalidResult.setActive(false);
-        invalidResult.setExpiresAt("2019-05-30T10:15:30+01:00");
-//        invalidResult.setContext(Map.of("email", "john.doe@example.com"));
-        invalidResult.setWwwAuthenticate("Basic realm=\"example.com\"");
-        return invalidResult;
-    }
 
 }
